c32f0903926399c34c9f38ee6c0d821aa1eee5d9
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-04-29 17:16:34 +0200

    GET endpoints working

--- backend/dto/dto.py
+++ backend/dto/dto.py
@@ -4,18 +4,38 @@ from pydantic import BaseModel
 class Customer(BaseModel):
     id: int
     name: str
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
 
 class Job(BaseModel):
     id: int
     name: str
     description: str
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
     customer_id: int
 
 class Document(BaseModel):
     id: int
     name: str
     description: str
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
     job_id: int
+
+class Revision(BaseModel):
+    id: int
+    version: str
+    description: str    
+    file_path: str
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
+    user_id: int
+    document_id: int    
 
 class User(BaseModel):
     id: int
@@ -27,12 +47,3 @@ class User(BaseModel):
     updated_at: datetime
     deleted_at: datetime | None = None
     access_level_id: int
-
-class Revision(BaseModel):
-    id: int
-    version: int
-    description: str
-    timestamp: datetime
-    file_path: str
-    user_id: int
-    document_id: int
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from os import getcwd
 from typing import List
-from backend.dto.dto import User
+from backend.dto.dto import User, Customer, Job, Document, Revision
 
 # Constants declaration and initialization
 WORKING_DIR: str = getcwd()
@@ -33,41 +33,40 @@ def root(request: Request) -> HTMLResponse:
 @app.get('/users', description='Get list of configured Users')
 async def get_users() -> List[User]:
 
-    # Creazione di un dizionario per trasformare la lista di liste in una lista di dizionari    
+    # Creating a dictionary to use it as a container for User data
     d: dict = {}
 
-    # Lista di utenti
+    # Initialize empty list of users
     users: List[User] = []
 
-    # Query di SELECT
     query: str = '''SELECT * 
                     FROM Users 
                     WHERE Users.deleted_at IS NULL 
                     ORDER BY Users.name'''
 
-    # Inizializzazione della connessione con il DB SQLite
+    # Inizialize SQLite db connection
     with sqlite3.connect(DATABASE_DIR) as connection:
         
-        # Ottenimento del cursore SQLite3
+        # Obtaining cursor from db
         cursor = connection.execute(query)
 
-        # Ottenimento dei records
+        # Obtaining records from table
         rows = cursor.fetchall()
 
-        # Ottenimento dei nomi delle colonne del risultato della query
+        # Obtaining columns names from the cursor
         column_names = [c[0] for c in cursor.description]
 
-        # Per ogni riga nel risultato della query
+        # For each row in the query result
         for r in rows:
             
-            # zip dei nomi delle colonne con i valori delle stesse
+            # zipping column name with the corresponding value
             r = list(zip(column_names, r))
             
-            # Per ogni tupla zippata (k, v), creazione del dizionario con l'associazione k = v
+            # For each zipped tuple (k, v), creating a dictionary with the association k:v
             for t in r:
                 d[t[0]] = t[1]
             
-            # Unpacking dei valori del dizionario
+            # Unpacking dictionary values
             id, name, surname, email, password, created_at, updated_at, deleted_at, access_level_id  = d.values()
             user: User = User(id=id, 
                             name=name, 
@@ -79,6 +78,219 @@ async def get_users() -> List[User]:
                             deleted_at=deleted_at, 
                             access_level_id=access_level_id)
 
+            # Append the User to the list of Users
             users.append(user)
     
         return users
+    
+
+# GET /customers
+@app.get('/customers', description='Get list of configured Customers')
+async def get_customers() -> List[Customer]:
+
+    # Creating a dictionary to use it as a container for Customer data
+    d: dict = {}
+
+    # Initialize empty list of customers
+    customers: List[Customer] = []
+
+    query: str = '''SELECT * 
+                    FROM Customers 
+                    WHERE Customers.deleted_at IS NULL 
+                    ORDER BY Customers.name'''
+
+    # Inizialize SQLite db connection
+    with sqlite3.connect(DATABASE_DIR) as connection:
+        
+        # Obtaining cursor from db
+        cursor = connection.execute(query)
+
+        # Obtaining records from table
+        rows = cursor.fetchall()
+
+        # Obtaining columns names from the cursor
+        column_names = [c[0] for c in cursor.description]
+
+        # For each row in the query result
+        for r in rows:
+            
+            # zipping column name with the corresponding value
+            r = list(zip(column_names, r))
+            
+            # For each zipped tuple (k, v), creating a dictionary with the association k:v
+            for t in r:
+                d[t[0]] = t[1]
+            
+            # Unpacking dictionary values
+            id, name, created_at, updated_at, deleted_at = d.values()
+            customer: Customer = Customer(id=id, 
+                                      name=name,                             
+                                      created_at=created_at, 
+                                      updated_at=updated_at, 
+                                      deleted_at=deleted_at)
+
+            # Append the Customer to the list of Customers
+            customers.append(customer)
+    
+        return customers
+    
+
+# GET /jobs
+@app.get('/jobs', description='Get list of configured Jobs')
+async def get_jobs() -> List[Job]:
+
+    # Creating a dictionary to use it as a container for Job data
+    d: dict = {}
+
+    # Initialize empty list of jobs
+    jobs: List[Job] = []
+
+    query: str = '''SELECT * 
+                    FROM Jobs 
+                    WHERE Jobs.deleted_at IS NULL
+                    ORDER BY Jobs.name'''
+
+    # Inizialize SQLite db connection
+    with sqlite3.connect(DATABASE_DIR) as connection:
+        
+        # Obtaining cursor from db
+        cursor = connection.execute(query)
+
+        # Obtaining records from table
+        rows = cursor.fetchall()
+
+        # Obtaining columns names from the cursor
+        column_names = [c[0] for c in cursor.description]
+
+        # For each row in the query result
+        for r in rows:
+            
+            # zipping column name with the corresponding value
+            r = list(zip(column_names, r))
+            
+            # For each zipped tuple (k, v), creating a dictionary with the association k:v
+            for t in r:
+                d[t[0]] = t[1]
+            
+            # Unpacking dictionary values
+            id, name, description, created_at, updated_at, deleted_at, customer_id = d.values()
+            job: Job = Job(id=id, 
+                           name=name,
+                           description=description,                         
+                           created_at=created_at, 
+                           updated_at=updated_at, 
+                           deleted_at=deleted_at,
+                           customer_id=customer_id)
+
+            # Append the Customer to the list of Jobs
+            jobs.append(job)
+    
+        return jobs
+    
+
+# GET /documents
+@app.get('/documents', description='Get list of configured Documents')
+async def get_documents() -> List[Document]:
+
+    # Creating a dictionary to use it as a container for Document data
+    d: dict = {}
+
+    # Initialize empty list of documents
+    documents: List[Revision] = []
+
+    query: str = '''SELECT * 
+                    FROM Documents 
+                    WHERE Documents.deleted_at IS NULL
+                    ORDER BY Documents.name'''
+
+    # Inizialize SQLite db connection
+    with sqlite3.connect(DATABASE_DIR) as connection:
+        
+        # Obtaining cursor from db
+        cursor = connection.execute(query)
+
+        # Obtaining records from table
+        rows = cursor.fetchall()
+
+        # Obtaining columns names from the cursor
+        column_names = [c[0] for c in cursor.description]
+
+        # For each row in the query result
+        for r in rows:
+            
+            # zipping column name with the corresponding value
+            r = list(zip(column_names, r))
+            
+            # For each zipped tuple (k, v), creating a dictionary with the association k:v
+            for t in r:
+                d[t[0]] = t[1]
+            
+            # Unpacking dictionary values
+            id, name, description, created_at, updated_at, deleted_at, job_id = d.values()
+            document: Document = Document(id=id, 
+                                          name=name,
+                                          description=description,                         
+                                          created_at=created_at, 
+                                          updated_at=updated_at, 
+                                          deleted_at=deleted_at,
+                                          job_id=job_id)
+
+            # Append the Customer to the list of Documents
+            documents.append(document)
+    
+        return documents
+    
+
+# GET /revisions
+@app.get('/revisions', description='Get list of configured Revisions')
+async def get_revisions() -> List[Revision]:
+
+    # Creating a dictionary to use it as a container for Revision data
+    d: dict = {}
+
+    # Initialize empty list of revisions
+    revisions: List[Revision] = []
+
+    query: str = '''SELECT * 
+                    FROM Revisions 
+                    WHERE Revisions.deleted_at IS NULL
+                    ORDER BY Revisions.id'''
+
+    # Inizialize SQLite db connection
+    with sqlite3.connect(DATABASE_DIR) as connection:
+        
+        # Obtaining cursor from db
+        cursor = connection.execute(query)
+
+        # Obtaining records from table
+        rows = cursor.fetchall()
+
+        # Obtaining columns names from the cursor
+        column_names = [c[0] for c in cursor.description]
+
+        # For each row in the query result
+        for r in rows:
+            
+            # zipping column name with the corresponding value
+            r = list(zip(column_names, r))
+            
+            # For each zipped tuple (k, v), creating a dictionary with the association k:v
+            for t in r:
+                d[t[0]] = t[1]
+            
+            # Unpacking dictionary values
+            id, version, description, file_path, created_at, updated_at, deleted_at, user_id, document_id = d.values()
+            revision: Revision = Revision(id=id, 
+                                          version=version,
+                                          description=description,
+                                          file_path=file_path,                      
+                                          created_at=created_at, 
+                                          updated_at=updated_at, 
+                                          deleted_at=deleted_at,
+                                          user_id=user_id,
+                                          document_id=document_id)
+
+            # Append the Customer to the list of Documents
+            revisions.append(revision)
+    
+        return revisions
